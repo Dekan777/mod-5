6583c53e46d388155eeba9345a5bdebaf83488d3
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-30 12:07:39 +0100

    e

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,24 @@
-import { Routes, Route, NavLink } from 'react-router-dom';
-import styled from 'styled-components';
-import { Home } from './Home/Home';
-import { About } from './About/About';
-import { Products } from './Products/Products';
-
-const StyledLink = styled(NavLink)`
-  color: black;
-
-  &.active {
-    color: orange;
-  }
-`;
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from 'path/to/pages/Home';
+import About from 'path/to/pages/About';
+import Products from 'path/to/pages/Products';
+import NotFound from 'path/to/pages/NotFound';
+import ProductDetails from 'path/to/pages/ProductDetails';
 
 export const App = () => {
   return (
     <div>
       <nav>
-        <StyledLink to="/" end>
-          Home
-        </StyledLink>
-        <StyledLink to="/about">About</StyledLink>
-        <StyledLink to="/products">Products</StyledLink>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/products">Products</Link>
       </nav>
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/products/:productId" element={<ProductDetails />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );

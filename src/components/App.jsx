344540dd19fe0490f10b576a359c26d5f5b8cb23
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-30 11:57:52 +0100

    link

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,33 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import { Home } from './Home/Home';
+import { About } from './About/About';
+import { Products } from './Products/Products';
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
+
 export const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <nav>
+        <StyledLink to="/" end>
+          Home
+        </StyledLink>
+        <StyledLink to="/about">About</StyledLink>
+        <StyledLink to="/products">Products</StyledLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </div>
+  );
 };

e47c1f675e4c279215887af22a64b27a40b6a368
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-30 11:09:30 +0100

    start

--- src/components/ErrorMessage/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-import css from './ErrorMessage.module.css';
-
-export const ErrorMessage = () => {
-  return (
-    <p className={css.error}>
-      Whoops, something went wrong! Please try reloading this page!
-    </p>
-  );
-};
